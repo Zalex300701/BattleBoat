a490b3d6ad9a72f488ee3f0524271d3f0a2f64f2
Zalex300701 <alexandre.dubuc@hotmail.fr>
2025-04-12 12:19:09 +0200

    slide on load scene
    v0.1

--- boat-battle/scripts/game_menu/gm_player.gd
+++ boat-battle/scripts/game_menu/gm_player.gd
@@ -1,12 +1,18 @@
 extends CharacterBody3D
 
 # Buoyancy variables
-var bobbing_amplitude: float = 0.2 # Height variation
+var bobbing_amplitude: float = 0.2
 var bobbing_speed: float = 2.0
 var rotation_amplitude: float = 2.0
 var rotation_speed: float = 1.5
 var initial_y: float
 var initial_rotation: Vector3
+
+# Transition
+var is_leaving := false
+var leave_speed := 3.0
+var leave_direction := Vector3.FORWARD
+var scene_to_load := "res://scenes/game.tscn"
 
 func _ready():
 	initial_y = global_transform.origin.y
@@ -15,14 +21,24 @@ func _ready():
 func _physics_process(delta):
 	apply_buoyancy(delta)
 
+	if is_leaving:
+		position += -global_transform.basis.z * leave_speed * delta  # Move forward
+		if global_transform.origin.z > 50:  # or whatever "out of frame" means
+			get_tree().change_scene_to_file(scene_to_load)
+
 func apply_buoyancy(delta):
-	# Vertical bobbing (apply as part of velocity)
+	if is_leaving:
+		return  # Optional: stop bobbing while leaving
+
 	var bobbing_offset = sin(Time.get_ticks_msec() * 0.001 * bobbing_speed) * bobbing_amplitude
 	velocity.y = (initial_y + bobbing_offset - global_transform.origin.y) / delta
-	
-	# Roll and pitch
+
 	var roll = sin(Time.get_ticks_msec() * 0.001 * rotation_speed) * rotation_amplitude
 	var pitch = cos(Time.get_ticks_msec() * 0.001 * rotation_speed) * rotation_amplitude
 	
 	rotation_degrees.x = initial_rotation.x + roll
 	rotation_degrees.z = initial_rotation.z + pitch
+
+func leave_and_start(scene_path: String):
+	is_leaving = true
+	scene_to_load = scene_path
--- boat-battle/scripts/game_menu/gm_ui.gd
+++ boat-battle/scripts/game_menu/gm_ui.gd
@@ -1,14 +1,7 @@
 extends Node
 
+@onready var player = $"../Environment/Player"
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+func _on_start_pressed():
+	if player and player.has_method("leave_and_start"):
+		player.leave_and_start("res://scenes/game.tscn")
