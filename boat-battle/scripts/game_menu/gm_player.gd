0b6819929782fff2764e25d1d5963f80cd6fa3a6
Zalex300701 <alexandre.dubuc@hotmail.fr>
2025-04-12 19:21:40 +0200

    more ui and game menu set up

--- boat-battle/scripts/game_menu/gm_player.gd
+++ boat-battle/scripts/game_menu/gm_player.gd
@@ -10,7 +10,7 @@ var initial_rotation: Vector3
 
 # Transition
 var is_leaving := false
-var leave_speed := 3.0
+var leave_speed := 5.0
 var leave_direction := Vector3.FORWARD
 var scene_to_load := "res://scenes/game.tscn"
 
@@ -23,13 +23,10 @@ func _physics_process(delta):
 
 	if is_leaving:
 		position += -global_transform.basis.z * leave_speed * delta  # Move forward
-		if global_transform.origin.z > 50:  # or whatever "out of frame" means
+		if global_transform.origin.z < -20:  # or whatever "out of frame" means
 			get_tree().change_scene_to_file(scene_to_load)
 
 func apply_buoyancy(delta):
-	if is_leaving:
-		return  # Optional: stop bobbing while leaving
-
 	var bobbing_offset = sin(Time.get_ticks_msec() * 0.001 * bobbing_speed) * bobbing_amplitude
 	velocity.y = (initial_y + bobbing_offset - global_transform.origin.y) / delta
 
--- boat-battle/scripts/game_menu/gm_ui.gd
+++ boat-battle/scripts/game_menu/gm_ui.gd
@@ -5,3 +5,9 @@ extends Node
 func _on_start_pressed():
 	if player and player.has_method("leave_and_start"):
 		player.leave_and_start("res://scenes/game.tscn")
+
+func _on_boathouse_button_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_exit_button_pressed() -> void:
+	pass # Replace with function body.
