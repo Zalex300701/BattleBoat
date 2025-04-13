6726240c1cebb97ac7e8545aced8832a0961b798
Zalex300701 <alexandre.dubuc@hotmail.fr>
2025-04-13 17:53:10 +0200

    boathouse cam position + exit button

--- boat-battle/scripts/game_menu/gm_camera.gd
+++ boat-battle/scripts/game_menu/gm_camera.gd
@@ -1,11 +1,29 @@
-extends Node
+extends Camera3D
 
+@onready var player: CharacterBody3D = get_node("/root/GameMenu/Environment/Player") 
+@onready var menu_marker: Marker3D = get_node("/root/GameMenu/Markers/Menu_marker")
+@onready var boathouse_marker: Marker3D = get_node("/root/GameMenu/Markers/Boathouse_marker")
 
-# Called when the node enters the scene tree for the first time.
+var target_position: Vector3
+var is_transitioning: bool = false
+
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
+
+func _process(delta: float) -> void:
+	if !player.is_leaving:
+		look_at(player.global_transform.origin + Vector3(0, 3, 0))
+	
+	if is_transitioning:
+		position = position.lerp(target_position, delta * 2)  # 2 = vitesse, ajuste comme tu veux
+		if position.distance_to(target_position) < 0.05:
+			position = target_position
+			is_transitioning = false
+
+func boathouse_position():
+	target_position = boathouse_marker.position
+	is_transitioning = true
+
+func menu_position():
+	target_position = menu_marker.position
+	is_transitioning = true
--- boat-battle/scripts/game_menu/gm_ui.gd
+++ boat-battle/scripts/game_menu/gm_ui.gd
@@ -1,13 +1,15 @@
 extends Node
 
 @onready var player = $"../Environment/Player"
+@onready var camera = $"../Camera3D"
 
 func _on_start_pressed():
 	if player and player.has_method("leave_and_start"):
 		player.leave_and_start("res://scenes/game.tscn")
 
 func _on_boathouse_button_pressed() -> void:
-	pass # Replace with function body.
+	if camera and camera.has_method("boathouse_position"):
+		camera.boathouse_position()
 
 func _on_exit_button_pressed() -> void:
-	pass # Replace with function body.
+	get_tree().quit()
